0b045d9486c71cca9449d8d64f434633d5b5f0f4
James Ian Fauni <james.fauni@maximintegrated.com>
2018-05-24 18:57:33 +0800

    animation at heart

--- app/src/main/java/com/lagotronicsprojects/birdslight/SplashscreenActivity.java
+++ app/src/main/java/com/lagotronicsprojects/birdslight/SplashscreenActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 import com.richpath.RichPath;
 import com.richpath.RichPathView;
@@ -20,5 +22,27 @@ public class SplashscreenActivity extends Activity {
         setContentView(R.layout.activity_splashscreen);
 
 
+
+        View img = findViewById(R.id.heart_vector);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RichPathView img = findViewById(R.id.heart_vector);
+                RichPath outline = img.findRichPathByName("outline");
+                RichPathAnimator.animate(outline)
+                        .interpolator(new AccelerateDecelerateInterpolator())
+                        .trimPathEnd(0,1)
+                        .duration(4000)
+                        .andAnimate(outline)
+                        .strokeColor(Color.rgb(0x5D,0x5D,0x5D),Color.blue(255), Color.rgb(0x5D,0x5D,0x5D))
+                        .duration(4000)
+                        .startDelay(50)
+
+                        .start();
+            }
+        });
+
     }
+
 }
